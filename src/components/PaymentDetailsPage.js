f3e35f07b795f1fd1e0d9ef9ab480e1fb3df642e
pelumi <coltcopy@gmail.com>
2024-03-27 12:20:41 +0100

    confirm payment

--- src/components/PaymentDetailsPage.js
+++ src/components/PaymentDetailsPage.js
@@ -15,11 +15,15 @@ const PaymentDetailsPage = () => {
 
   return (
     <div className="formDiv" id="paymentDetailsPageDiv">
-      <h1 className={`fade-up-h1 ${loaded ? 'fade-up' : ''}`}>Registration Almost Complete!</h1>
-      <p className={`fade-up-p ${loaded ? 'fade-up' : ''}`}>Kindly pay N15,000 to the following bank details to secure your camping spot</p>
+      <h1 className={`fade-up-h1 ${loaded ? 'fade-up' : ''}`}>Registration Complete!</h1>
+      <p className={`fade-up-p ${loaded ? 'fade-up' : ''}`}>We await your payment</p>
       <p>Bank name: Wema Bank</p>
       <p>Account number: 0268786368</p>
       <p>Account name: Blessing Aniefiok</p>
+      <div className='moreDetailsDiv'>
+        <small className={`fade-up-h1 ${loaded ? 'fade-up' : ''}`}>To confirm your payment</small>
+        <a href='https://api.whatsapp.com/send?phone=2348028370487&text=Hi%2C%20I%27d%20like%20to%20enquire%20about%20Peniel' className={`fade-up-h1 ${loaded ? 'fade-up' : ''}`}>Reach out via whatsapp</a> <br />
+      </div>
       {/* <Link to="/">Home</Link> */}
     </div>
   );
