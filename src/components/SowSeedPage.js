811dfb17ff458fe1658d9b27579c7c8488040c14
DA <65969343+frontendrockstarng@users.noreply.github.com>
2026-01-24 16:04:17 +0100

    Update SowSeedPage.js

--- src/components/SowSeedPage.js
+++ src/components/SowSeedPage.js
@@ -17,8 +17,8 @@ const SowSeedPage = () => {
     <div className="formDiv" id="paymentDetailsPageDiv">
       <h1 className={`fade-up-h1 ${loaded ? 'fade-up' : ''}`}>Registration Complete!</h1>
       <p className={`fade-up-p ${loaded ? 'fade-up' : ''}`}>Kindly make payment to the following bank details to sow towards PENIEL</p>
-      <p>Bank name: Wema Bank</p>
-      <p>Account number: 0268786368</p>
+      <p>Bank name: Vale Finance</p>
+      <p>Account number: 1010950696</p>
       <p>Account name: Blessing Aniefiok</p>
       <div className='moreDetailsDiv'>
         <small className={`fade-up-h1 ${loaded ? 'fade-up' : ''}`}>For further enquires</small>
